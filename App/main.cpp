6a85c6b142d8e45fcb695e1889ee99c210e2f9e6
djeada <addjellouli1@gmail.com>
2020-09-07 00:50:18 +0200

    added app icon

--- App/main.cpp
+++ App/main.cpp
@@ -5,6 +5,10 @@
 int main(int argv, char **args)
 {
     QApplication app(argv, args);
+    app.setApplicationName("Lightpad");
+    app.setOrganizationName("Lightpad");
+    app.setWindowIcon(QIcon(":/resources/icons/app.png"));
+
     MainWindow w;
 
     return app.exec();
