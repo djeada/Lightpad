c260ed5952675f0d225fc91e5d67ca06af84ed55
djeada <addjellouli1@gmail.com>
2021-02-03 17:56:25 +0100

    added LineEdit with Icon

--- App/runconfigurations.cpp
+++ App/runconfigurations.cpp
@@ -1,11 +1,63 @@
 #include "runconfigurations.h"
 #include "ui_runconfigurations.h"
 
+#include <QPainter>
+#include <QStyleOption>
+LineEditIcon::LineEditIcon(QWidget* parent)
+    : QLineEdit(parent)
+{
+
+    QHBoxLayout* mainLayout = new QHBoxLayout();
+    mainLayout->addWidget(&edit);
+    mainLayout->addWidget(&button);
+    mainLayout->setContentsMargins(0, 0, 0, 0);
+    mainLayout->setSpacing(0);
+    setLayout(mainLayout);
+    setStyleSheet("QLineEdit { border: none; background: white } QToolButton { background: white }");
+    setMouseTracking(true);
+    setAttribute(Qt::WA_Hover);
+
+}
+
+LineEditIcon::~LineEditIcon()
+{
+}
+
+void LineEditIcon::setIcon(QIcon icon)
+{
+    button.setIcon(icon);
+}
+
+void LineEditIcon::paintEvent(QPaintEvent* event)
+{
+    Q_UNUSED(event);
+  QStyleOption option;
+  option.initFrom(this);
+  QPainter painter(this);
+  style()->drawPrimitive(QStyle::PE_Widget, &option, &painter, this);
+};
+
+void LineEditIcon::enterEvent(QEvent* event)
+{
+    setStyleSheet("QLineEdit {border-width: 1px; border-style: solid; border-color: #add8e6; }"
+                  "QLineEdit#edit { border: none; background: white } QToolButton { background: white }");
+
+    QWidget::enterEvent(event);
+}
+
+void LineEditIcon::leaveEvent(QEvent* event)
+{
+    setStyleSheet("QLineEdit { border: none; background: white } QToolButton { background: white }");
+
+    QWidget::leaveEvent(event);
+}
+
 RunConfigurations::RunConfigurations(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::runconfigurations)
 {
     ui->setupUi(this);
+    ui->editScriptPath->setIcon(QIcon(":/resources/icons/folder.png"));
     show();
 }
 
--- App/runconfigurations.h
+++ App/runconfigurations.h
@@ -2,6 +2,27 @@
 #define RUNCOFIGURATIONS_H
 
 #include <QDialog>
+#include <QLineEdit>
+#include <QIcon>
+#include <QToolButton>
+
+class LineEditIcon : public QLineEdit
+{
+    Q_OBJECT
+
+public:
+    LineEditIcon(QWidget* parent = nullptr);
+    ~LineEditIcon();
+    void setIcon(QIcon icon);
+
+protected:
+    virtual void paintEvent(QPaintEvent *pe) override;
+private:
+    QLineEdit edit;
+    QToolButton button;
+    virtual void enterEvent(QEvent* event) override;
+    virtual void leaveEvent(QEvent* event) override;
+};
 
 namespace Ui {
 class runconfigurations;
