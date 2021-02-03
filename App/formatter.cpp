95c223306b48fffae64fe16c15168ee13963343b
djeada <addjellouli1@gmail.com>
2021-02-03 19:56:20 +0100

    added formatter class

---
+++ App/formatter.cpp
@@ -0,0 +1,6 @@
+#include "formatter.h"
+
+Formatter::Formatter()
+{
+
+}
---
+++ App/formatter.h
@@ -0,0 +1,11 @@
+#ifndef FORMATTER_H
+#define FORMATTER_H
+
+
+class Formatter
+{
+public:
+    Formatter();
+};
+
+#endif // FORMATTER_H
